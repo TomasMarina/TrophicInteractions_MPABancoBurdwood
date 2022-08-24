669308f5ba9979b370c7cfe35db62fd15cfddf4f
TomasMarina <tomasimarina@gmail.com>
2022-08-24 16:59:51 -0300

    Figure details

--- R/5_figures.R
+++ R/5_figures.R
@@ -6,7 +6,7 @@
 # Paquetes ----
 
 packages <- c("tidyverse", "ggplot2", "igraph", "multiweb", "scales",
-              "ggjoy")
+              "ggjoy", "ggpubr")
 ipak <- function(pkg){
   new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
   if (length(new.pkg))
@@ -28,37 +28,56 @@ load("results/summary_results.rda")
 # Red trófica por degree
 plot_troph_level(g_up)
 
+# Distribución de grado
+# Histograma
+degree <- as.data.frame(degree(g_up, mode = "total"))
+(plot_distdegree <- ggplot(degree, aes(degree[,1])) +
+    geom_histogram(bins = 100, alpha = 0.3, color = "black") +
+    labs(x = "Degree", y = "Frequency") +
+    theme_bw() +
+    theme(legend.position = "none",
+          axis.text.x = element_text(size = 14),
+          axis.text.y = element_text(size = 14),
+          axis.title.x = element_text(face = "bold", size = 16),
+          axis.title.y = element_text(face = "bold", size = 16)))
+
 # Generality (generalistas vs especialistas)
-(plot_indeg <- spp_total %>% 
+plot_indeg <- spp_total %>% 
     filter(NumPrey != 0) %>% 
     ggplot(., aes(x = NumPrey)) +
     geom_histogram(stat = "count") +
     labs(x = "Predators", y = "Number of prey") +
+    theme_bw() +
     theme(legend.position = "none",
           axis.text.x = element_text(size = 14),
           axis.text.y = element_text(size = 14),
           axis.title.x = element_text(face = "bold", size = 16),
-          axis.title.y = element_text(face = "bold", size = 16)) +
-annotate("text", x = Inf, y = Inf, label = paste("Prey per predator = ", round(prop.topol$Generality, 2), sep = ""),
-size = 6, vjust=1, hjust=1))
+          axis.title.y = element_text(face = "bold", size = 16)) #+
+# annotate("text", x = Inf, y = Inf, label = paste("Prey per predator = ", round(prop.topol$Generality, 2), sep = ""),
+#          size = 5, vjust=1, hjust=1))
+plot_indeg
 
 # Vulnerability (flujos de energía)
-(plot_outdeg <- spp_total %>% 
+plot_outdeg <- spp_total %>% 
     filter(NumPred != 0) %>% 
     ggplot(., aes(x = NumPred)) +
-    geom_histogram(stat = "count") +
+    geom_histogram(stat = "count", bins = 50) +
     labs(x = "Prey", y = "Number of predators") +
+    theme_bw() +
     theme(legend.position = "none",
           axis.text.x = element_text(size = 14),
           axis.text.y = element_text(size = 14),
           axis.title.x = element_text(face = "bold", size = 16),
-          axis.title.y = element_text(face = "bold", size = 16)) +
-annotate("text", x = Inf, y = Inf, label = paste("Predators per prey = ", round(prop.topol$Vulnerability, 2), sep = ""),
-         size = 6, vjust=1, hjust=1))
+          axis.title.y = element_text(face = "bold", size = 16)) #+
+# annotate("text", x = Inf, y = Inf, label = paste("Predators per prey = ", round(prop.topol$Vulnerability, 2), sep = ""),
+#          size = 5, vjust=1, hjust=1))
+plot_outdeg
 
+gen_vul_plot <- ggarrange(plot_indeg, plot_outdeg,
+                      labels = c("A", "B"), ncol=2, nrow=1)
+gen_vul_plot
 
-
-## Escala subgrupos ----
+# Distribución interacciones por GF
 
 load("data/foodweb-data_ago22.rda")
 
@@ -80,7 +99,7 @@ plot_distdegree_fg <- spp_total %>%
     ggplot(., aes(x = TotalDegree, y = FunctionalGroup, fill = FunctionalGroup)) + 
     geom_joy(scale = 3.75) +
     scale_fill_manual(values = FGColors) +
-    labs(x = "Número de interacciones", y = "Grupo funcional") +
+    labs(x = "Number of interactions", y = "Functional group") +
     theme_joy(grid = FALSE) +
     theme(legend.position = "none",
           axis.text.x = element_text(size = 14),
@@ -90,17 +109,68 @@ plot_distdegree_fg <- spp_total %>%
 plot_distdegree_fg
 
 # Red trófica por grupo funcional 'FG'
-spp_total <- spp_total %>% 
-  dplyr::select(FunctionalGroup, everything()) %>% 
-  left_join(unique_FG)
-g_up <- g_up %>% 
-  set_vertex_attr("FGColor", value = spp_total$FGColor)
+# spp_total <- spp_total %>% 
+#   dplyr::select(FunctionalGroup, everything()) %>% 
+#   left_join(unique_FG)
+# g_up <- g_up %>% 
+#   set_vertex_attr("FGColor", value = spp_total$FGColor)
+# 
+# plot_troph_level(g_up, vertex.color = V(g_up)$FGColor)
+# legend("bottom", legend=unique_FG$FunctionalGroup, col = unique_FG$FGColor, ncol = 5,
+#        bty = "n", pch=20 , pt.cex = 3, cex = 1, inset=c(-0.2,0))
 
-plot_troph_level(g_up, vertex.color = V(g_up)$FGColor)
-legend("bottom", legend=unique_FG$FunctionalGroup, col = unique_FG$FGColor, ncol = 5,
-       bty = "n", pch=20 , pt.cex = 3, cex = 1, inset=c(-0.2,0))
 
-# Roles topológicos
+## Escala especie ----
+
+### Distribución de grado ----
+
+spp_nt_12 <- length(spp_total$TL[spp_total$TL < 2])
+spp_nt_13 <- length(spp_total$TL[spp_total$TL >= 2 & spp_total$TL < 3])
+spp_nt_14 <- length(spp_total$TL[spp_total$TL < 4])
+
+plot_sp_tl <- ggplot(spp_total, aes(x = reorder(TrophicSpecies, TL), y = TotalDegree)) +
+    geom_point() +
+    # geom_smooth(aes(as.numeric(reorder(TrophicSpecies, TL)), Degree), method = "loess") +
+    geom_vline(xintercept = c(spp_nt_12+1, spp_nt_12+spp_nt_13+1, spp_nt_14+1), linetype = "longdash", colour = "red") +
+    labs(x = "Species (increasing TL)", y = "Number of interactions") +
+    theme_bw() +
+    theme(panel.grid = element_blank(),
+          axis.title = element_text(size = 18, face = "bold"),
+          axis.text.x = element_blank(),
+          axis.text.y = element_text(size = 15))
+plot_sp_tl
+
+
+### Presas y depredadores ----
+
+data_plot_sp_tl <- spp_total %>% 
+  dplyr::select(-c(Omn, meanTrophicSimil)) %>% 
+  gather(type, count, NumPrey:NumPred) 
+
+#hue_pal()(2)  # get color code
+plot_sp_tl_bar <- ggplot(data_plot_sp_tl, aes(x = reorder(TrophicSpecies, TL), y = count, 
+                                               fill = forcats::fct_rev(type))) +
+    geom_bar(stat="identity") +
+    scale_fill_manual(values = c("#F8766D","#00BFC4"), labels = c("Prey", "Predator")) +
+    labs(x = "Species (increasing TL)", y = "Number of interactions") +
+    guides(fill = guide_legend(title = "Number of:")) +
+    theme_bw() +
+    theme(panel.grid = element_blank(),
+          axis.title = element_text(size = 12, face = "bold"),
+          axis.text.x = element_blank())
+plot_sp_tl_bar
+
+# Gráfico interactivo
+library(plotly)
+pr_pred_int <- spp_total %>% 
+  filter(TotalDegree > 30) %>%
+  plot_ly(., x = ~reorder(TrophicSpecies, -TotalDegree), y = ~NumPrey, type = 'bar', name = 'NumPrey') %>%
+  add_trace(y = ~NumPred, name = 'NumPred') %>%
+  layout(yaxis = list(title = 'Number of interactions'), 
+         xaxis = list(title = 'Species'), barmode = 'stack')
+pr_pred_int
+
+### Roles topológicos ----
 
 top.role.col <- hcl.colors(4, palette = "Purple-Green")
 TRColor <- setNames(top.role.col, levels(as.factor(V(g_up)$TopRole)))
@@ -113,55 +183,6 @@ g_up_c <- g_up %>%
   set_vertex_attr("TRColor", value = data_TRColor$TRColor)
 
 plot_troph_level(g_up_c, weights = NA, vertex.color = V(g_up_c)$TRColor)
-
-
-## Escala especie ----
-
-### Distribución de grado ----
-spp_nt_12 <- length(spp_total$TL[spp_total$TL < 2])
-spp_nt_13 <- length(spp_total$TL[spp_total$TL >= 2 & spp_total$TL < 3])
-spp_nt_14 <- length(spp_total$TL[spp_total$TL < 4])
-
-(plot_sp_tl <- ggplot(spp_total, aes(x = reorder(TrophicSpecies, TL), y = TotalDegree)) +
-    geom_point() +
-    # geom_smooth(aes(as.numeric(reorder(TrophicSpecies, TL)), Degree), method = "loess") +
-    geom_vline(xintercept = c(spp_nt_12+1, spp_nt_12+spp_nt_13+1, spp_nt_14+1), linetype = "longdash", colour = "red") +
-    labs(x = "Species (increasing TL)", y = "Number of interactions") +
-    theme_bw() +
-    theme(panel.grid = element_blank(),
-          axis.title = element_text(size = 18, face = "bold"),
-          axis.text.x = element_blank(),
-          axis.text.y = element_text(size = 15)))
-
-
-### Presas y depredadores ----
-data_plot_sp_tl <- spp_total %>% 
-  dplyr::select(-c(Omn, meanTrophicSimil)) %>% 
-  gather(type, count, NumPrey:NumPred) 
-
-#hue_pal()(2)  # get color code
-(plot_sp_tl_bar <- ggplot(data_plot_sp_tl, aes(x = reorder(TrophicSpecies, TL), y = count, 
-                                               fill = forcats::fct_rev(type))) +
-    geom_bar(stat="identity") +
-    scale_fill_manual(values = c("#F8766D","#00BFC4"), labels = c("Prey", "Predator")) +
-    labs(x = "Species (increasing TL)", y = "Number of interactions") +
-    guides(fill = guide_legend(title = "Number of:")) +
-    theme_bw() +
-    theme(panel.grid = element_blank(),
-          axis.title = element_text(size = 12, face = "bold"),
-          axis.text.x = element_blank()))
-
-# Gráfico interactivo
-library(plotly)
-pr_pred_int <- spp_total %>% 
-  filter(TotalDegree > 30) %>%
-  plot_ly(., x = ~reorder(TrophicSpecies, -TotalDegree), y = ~NumPrey, type = 'bar', name = 'NumPrey') %>%
-  add_trace(y = ~NumPred, name = 'NumPred') %>%
-  layout(yaxis = list(title = 'Number of interactions'), 
-         xaxis = list(title = 'Species'), barmode = 'stack')
-pr_pred_int
-
-
 
 
 
